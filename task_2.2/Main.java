798d17350ee18942c83d42e483d6fc31aa2fe2c7
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-15 16:58:19 +0300

    Create Main.java

---
+++ task_2.2/Main.java
@@ -0,0 +1,17 @@
+/*Выведи на экран таблицу умножения 10 на 10 в следующем виде:
+1 2 3 4 ...
+2 4 6 8 ...
+3 6 9 12 ...
+4 8 12 16 ...
+... */
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++){
+            for (int k = 1; k < 11; k++){
+                System.out.print(k * i + " ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
