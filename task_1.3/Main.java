5b5923daa75497cc48cabebbf9e7e467910f88d0
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-15 15:14:15 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,12 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 25;
+        String s = "Площадь";
+        System.out.println(s + " = " + (a * b));
+
+    }
+}
