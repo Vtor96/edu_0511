0fcffb132ef2c92441aedc2fe1ab75a2d7cffc60
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-15 15:15:15 +0300

    Create Main.java

---
+++ task_1.4/Main.java
@@ -0,0 +1,16 @@
+/* Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 25;
+        System.out.println("Квадрат number равен " + (number * number));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 25;
+        System.out.println("Квадрат number равен " +  Math.pow(number, 2));
+    }
+}
