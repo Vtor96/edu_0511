7f9e05c2c7b72f3de27b4921a2011251f4edf26d
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-16 23:02:53 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,14 @@
+/*
+Задание: Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение элементов этого массива.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = 1;
+        int[] elem= {2, 3, 4, 5};
+        for(int i = 0; i < elem.length; i++){
+            sum = sum * elem[i];
+        }
+        System.out.println("Произведение элементов в массиве = " + sum);
+    }
+}
