47071bf85dbd26c73cbf88963e55451e60c5b47b
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-16 23:19:59 +0300

    Create Main.java

---
+++ task_2.9/Main.java
@@ -0,0 +1,21 @@
+/*
+Задание: Дан массив с числами. Проверьте, что в этом массиве есть число 5.
+Если есть - выведите 'да', а если нет - выведите 'нет'.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+            System.out.println("Массив: " + array[i]);
+
+            if (array[i] == 5) {
+                System.out.println("Да!");
+            } else {
+                System.out.println("Нет");
+            }
+        }
+    }
+}
