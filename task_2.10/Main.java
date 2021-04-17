eda1be86c588002848790a4303b02482158d12f3
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-17 14:59:25 +0300

    Create Main.java

---
+++ task_2.10/Main.java
@@ -0,0 +1,22 @@
+/*
+Задание: Дан массив с числами. Проверьте, есть ли в нем два одинаковых числа подряд.
+Если есть - выведите 'да', а если нет - выведите 'нет'.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int[] mass = new int[10];
+        //int[] mass = {5, 7, 9, 4, 5, 5, 8, 5, 1};
+
+        for (int i = 1; i < mass.length; i++) {
+            mass[i] = (int) (Math.random() * 10);
+
+            System.out.println("Массив: " + mass[i]);
+
+            if (mass[i] == mass[i-1])
+                System.out.println("Да!");
+            else
+                System.out.println("Нет");
+        }
+    }
+}
