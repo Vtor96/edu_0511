3c4bdf2f0ce3058623f8d6ec29fc97bbfe82d580
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-17 14:36:45 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -3,12 +3,16 @@
 */
 
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         System.out.print("Введите число = ");
         float a = scan.nextFloat();
+        
         double proc = a + ((15.0 * a) / 100);
+        
         System.out.println("Ваше увеличенное число = " + proc);
     }
 }
