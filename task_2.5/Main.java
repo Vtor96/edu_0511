8513e90ed536c0390271c3a8bf92d5b771b8da53
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-17 14:40:01 +0300

    Update Main.java

--- task_2.5/Main.java
+++ task_2.5/Main.java
@@ -8,8 +8,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         System.out.print("Введите число: ");
         int a = scanner.nextInt();
+        
         if (a == 10){
             System.out.println("Верно!");
         }else{
