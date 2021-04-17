619e6143be35c964b659c18041539a4553ce7585
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-17 14:39:23 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -8,17 +8,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         System.out.print("Введите сумму вклада: ");
         long vklad = scanner.nextLong();
+        
         System.out.print("Введите процент: ");
         long procent = scanner.nextLong();
 
         for (int year = 1; year <= 5; year++){
             vklad += ((vklad * procent)/100);
+            
             System.out.println("За " + year + " год на Вашем счету " + Math.round(vklad) + " рублей");
         }
-
-
-
     }
 }
