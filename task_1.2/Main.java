618b00544dd0a5a2d2ccc6416ebbb7e3de0c6ddf
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-19 22:37:34 +0300

    Update Main.java

--- task_1.2/Main.java
+++ task_1.2/Main.java
@@ -10,11 +10,11 @@ public class Main {
         int a = 3;
         int b = 2;
 
-        //System.out.print("два");
-        System.out.print(b);
+        System.out.print("два");
+        //System.out.print(b);
         System.out.print(" плюс ");
         //System.out.print(" минус ");
-        System.out.print(a);
+        //System.out.print(a);
         //System.out.print("три");
         System.out.print(" равно ");
         //System.out.print(" будет ");
