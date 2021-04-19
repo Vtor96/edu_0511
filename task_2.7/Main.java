3bcb7839d93e20a0546cb89d2bc53e9cfe08fb50
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-19 22:49:52 +0300

    Update Main.java

--- task_2.7/Main.java
+++ task_2.7/Main.java
@@ -4,15 +4,20 @@
 Если число не делится - выведите 'false', а если делится - выведите 'true'.
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число: ");
+        int num = scanner.nextInt();
         boolean f = true;
-        int num = 31;
-        
+
         for(int i = 2; i < num; i++ ){
             if((num % i) == 0) {
                 f = false;
-                continue;
+                break;
             }
         }
 
