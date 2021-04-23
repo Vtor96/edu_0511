c99a15123cd60d1e4ba50d5b8ea3b583425e0824
Vtor96 <82580116+Vtor96@users.noreply.github.com>
2021-04-23 11:34:57 +0300

    Update Main.java

--- task_4.1/Main.java
+++ task_4.1/Main.java
@@ -14,12 +14,16 @@ public class Main {
 
         cat.setName("Жужик");
 
-        System.out.println(cat.name);
+        System.out.println(cat.getName());
     }
 }
 
 class Cat {
     private String name = "безымянный кот";
+
+    public String getName() {
+        return name;
+    }
 
     public void setName(String name) {
         this.name = name;
